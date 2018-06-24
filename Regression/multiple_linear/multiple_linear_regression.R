acda859f393cb0637a25cbe48054e75af056ab97
Manish Basnet <mbasnet911@gmail.com>
2018-06-24 17:26:46 +0545

    importing dataset, encoding categorical data and splitting into training and test set

--- Regression/multiple_linear/multiple_linear_regression.R
+++ Regression/multiple_linear/multiple_linear_regression.R
@@ -5,11 +5,16 @@
 # Importing the dataset
 dataset = read.csv('50_Startups.csv')
 
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                      levels = c('New York', 'California','Florida'),
+                      labels = c(1,2,3))
+
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
