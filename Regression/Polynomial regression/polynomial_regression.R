947b87908d60922cbc79ac210d515d4fe804c632
Manish Basnet <mbasnet@growbydata.com>
2018-07-04 17:45:23 +0545

    fitting linear and polynomial regression to the dataset

--- Regression/Polynomial regression/polynomial_regression.R
+++ Regression/Polynomial regression/polynomial_regression.R
@@ -6,12 +6,27 @@ dataset = dataset[2:3]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$Profit, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Profit, SplitRatio = 0.8)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+# Fitting Linear Regression to the dataset
+lin_reg = lm(formula = Salary ~ .,
+             data = dataset)
+
+# Fitting Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+poly_reg = lm(formula = Salary ~ .,
+              data = dataset)
+
+
+
+
+
