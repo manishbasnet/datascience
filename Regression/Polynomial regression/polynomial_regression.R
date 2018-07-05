719686ecb8fdd71e08725cd779b2c044c836764b
Manish Basnet <mbasnet@growbydata.com>
2018-07-05 11:36:47 +0545

    predicting a new result with Linear, Polynomial Regression

--- Regression/Polynomial regression/polynomial_regression.R
+++ Regression/Polynomial regression/polynomial_regression.R
@@ -49,6 +49,30 @@ ggplot() +
   xlab('Level') + 
   ylab('Salary')
 
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
