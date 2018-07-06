4e61244c5188be4d3840e705f18d4685b177692e
Manish Basnet <mbasnet@growbydata.com>
2018-07-06 13:19:07 +0545

    random forest in python done

--- Regression/random_forest_regression/random_forest_regression.py
+++ Regression/random_forest_regression/random_forest_regression.py
@@ -24,7 +24,7 @@ y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting the Random Forest Regression to the dataset\
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
 regressor.fit(X, y)
 
 
@@ -33,7 +33,7 @@ y_pred = regressor.predict(6.5)
 
 
 # Visualizing the Random Forest Regression results (for higher resolution and smoother curve)
-X_grid = np.arrange(min(X), max(X), 0.1)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid),1))
 plt.scatter(X,y,color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
