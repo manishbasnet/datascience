fe27eff16f42271ab41a27b38c3ffaa4765f8481
Manish Basnet <mbasnet@growbydata.com>
2018-07-11 16:44:42 +0545

    data preprocessing step for logistic regression in R

--- Regression/Classification/logistic_regression/logistic_regression.R
+++ Regression/Classification/logistic_regression/logistic_regression.R
@@ -1,34 +1,17 @@
-# Data Preprocessing
+# Logistic Regression
 
 # Importing the dataset
-
-dataset = read.csv('Data.csv')
-# dataset = dataset[, 2:3]
+dataset = read.csv('Social_Network_Ads.csv')
+dataset = dataset[, 3:5]
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-# training_set[, 2:3] = scale(training_set[, 2:3])
-# test_set[, 2:3] = scale(test_set[, 2:3])
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
