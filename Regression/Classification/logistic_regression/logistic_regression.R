b8403995934fc255f047f9612212db45d9a575da
Manish Basnet <mbasnet@growbydata.com>
2018-07-12 11:16:45 +0545

    predicting the test set results in R

--- Regression/Classification/logistic_regression/logistic_regression.R
+++ Regression/Classification/logistic_regression/logistic_regression.R
@@ -21,7 +21,9 @@ classifier = glm(formula = Purchased ~ .,
                  family = binomial,
                  data = training_set)
 
-
+# Predicting the test set results
+prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
+y_pred = ifelse(prob_pred > 0.5, 1, 0)
 
 
 
