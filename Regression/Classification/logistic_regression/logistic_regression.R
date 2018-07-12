b5ccf1c5d24efae3072438b78e4c5a97aaf0d15f
Manish Basnet <mbasnet@growbydata.com>
2018-07-12 11:11:09 +0545

    fitting the test set results

--- Regression/Classification/logistic_regression/logistic_regression.R
+++ Regression/Classification/logistic_regression/logistic_regression.R
@@ -15,3 +15,15 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 training_set[, 1:2] = scale(training_set[, 1:2])
 test_set[, 1:2] = scale(test_set[, 1:2])
+
+# Fitting Logistic Regression to the Training set
+classifier = glm(formula = Purchased ~ .,
+                 family = binomial,
+                 data = training_set)
+
+
+
+
+
+
+
